89a9c12e810113223cd4ee00441145789d00c64c
Stephen Brown <stephen.jr36@gmail.com>
2024-08-20 13:20:17 -0600

    completed step 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 export default function App() {
+  const [friends, setFriends] = useState([]);
+  const [picture, setPicture] = useState('');
+  const [name, setName] = useState('');
+
   return (
     <div>
       <label htmlFor="picture">Picture:</label>
