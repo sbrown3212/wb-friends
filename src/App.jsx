3970c6b37ce9ecd6e6b666b5cdecd7577696c9d4
Stephen Brown <stephen.jr36@gmail.com>
2024-08-20 13:18:09 -0600

    completed step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <label htmlFor="picture">Picture:</label>
+      <input type="text" name="picture" id="picture" />
+
+      <label htmlFor="name">Name:</label>
+      <input type="text" name="name" id="name" />
+
+      <button>Add Friend</button>
+    </div>
+  );
 }
